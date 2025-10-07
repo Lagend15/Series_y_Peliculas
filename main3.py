87a5c5e59258c69c90c81aaaf46d16499358338c
JaimeTangarife <jaimetangarife92435@correo.itm.edu.co>
2025-10-06 19:14:09 -0500

    Se Termina de organizar el archivo main y se agregan algunos campos a la BD

--- main3.py
+++ main3.py
@@ -39,7 +39,6 @@ def main():
     # ==============================
     # EJEMPLOS DE CRUD
     # ==============================
-    # Descomenta uno a uno para probar cada operación
 
     # ------------------------------
     # ESTADOS
